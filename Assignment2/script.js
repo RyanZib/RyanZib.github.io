9e0d9c832df3cbe7ce7d0175810bd360c10916fe
RyanZib <131152478+RyanZib@users.noreply.github.com>
2024-05-18 22:24:03 +1000

    A2 day 1

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,7 +1,11 @@
 function removeOverlay() {
     var overlay = document.querySelector('.overlay');
+
     overlay.parentNode.removeChild(overlay);
+    var videoContainer = document.getElementById('videoContainer');
+    videoContainer.style.display = 'block';
     var video = document.getElementById('backgroundVideo');
+    video.play();
     if (video.requestFullscreen) {
         video.requestFullscreen().then(function() {
             video.play();
@@ -21,4 +25,29 @@ function removeOverlay() {
     }
     video.style.pointerEvents = 'auto';
 }
+ // Function to show content based on clicked list item
+ function showContent(page) {
+    // Hide the previously shown content
+    document.getElementById('content').style.display = 'none';
+    
+    // Show the new content based on the clicked list item
+    var content = document.getElementById('content');
+    switch (page) {
+      case 'Description':
+        content.innerHTML = '<h2>Video Art Piece</h2><p>This would contain information about the Video</p>';
+        break;
+      case 'Artist':
+        content.innerHTML = '<h2>Artist Name</h2><p>This would contain info on who the artist is</p>';
+        break;
+      case 'Other Works':
+        content.innerHTML = '<h2>other Works</h2><p>This would contain info about other art pieces the artist has done</p>';
+        break;
+      case 'Refrences':
+        content.innerHTML = '<h2>Page Refrences</h2><p>Learn more about us!</p>';
+        break;
+    }
+    
+    // Show the content
+    content.style.display = 'block';
+  }
 
